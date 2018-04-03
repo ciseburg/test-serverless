1e30a63ab93ca55b9afbf5261aed8d611532427c
ciseburg <iseburg@gmail.com>
2018-04-02 22:14:16 -0500

    updated lambda with SNS

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,19 +5,31 @@ import mimetypes
 import StringIO
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
     
-    build_bucket = s3.Bucket('kiranbuild.sgaslabs.net')
-    portfolio_bucket = s3.Bucket('kiran.sgaslabs.net')
+    try:
+        
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        sns = boto3.resource('sns')
+        topic = sns.Topic('arn:aws:sns:us-east-1:290093585298:deployPortfolioTopic')
     
-    #build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('kiranbuild.zip', portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-            
-    return 'Hello Guru'
+        build_bucket = s3.Bucket('kiranbuild.sgaslabs.net')
+        portfolio_bucket = s3.Bucket('kiran.sgaslabs.net')
+        
+        #build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('kiranbuild.zip', portfolio_zip)
+    
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+               
+        print "Job Done"
+        topic.publish(Subject='Deployed',Message='Portfolio Deployed Successfully')
+        
+    except:
+        topic.publish(Subject='Deploy failed',Message='Portfolio Deploy Failed')
+        raise
+    
+    return 'Lambda to Deploy Portfolio Done'
