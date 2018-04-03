7715ebb3a8a47e180a8c9a31804c1579e187d225
ciseburg <iseburg@gmail.com>
2018-04-02 23:59:21 -0500

    updating lambda for code pipeline

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,18 +6,32 @@ import StringIO
 
 def lambda_handler(event, context):
     
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    
     try:
+        location = {
+            "bucketName": 'kiranbuild.sgaslabs.net',
+            "objectKey": 'kiranbuild.zip'
+        }
         
-        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-        sns = boto3.resource('sns')
+        job = event.get("Codepipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact[name] == "MyAppBuild":
+                    location = artifact["location"]["s3location"]
+                   
+        print "Building portfolio from: " + str(location)
+        
         topic = sns.Topic('arn:aws:sns:us-east-1:290093585298:deployPortfolioTopic')
     
-        build_bucket = s3.Bucket('kiranbuild.sgaslabs.net')
+        build_bucket = s3.Bucket(location["bucketName"])
         portfolio_bucket = s3.Bucket('kiran.sgaslabs.net')
         
         #build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('kiranbuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
     
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -27,7 +41,9 @@ def lambda_handler(event, context):
                
         print "Job Done"
         topic.publish(Subject='Deployed',Message='Portfolio Deployed Successfully')
-        
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject='Deploy failed',Message='Portfolio Deploy Failed')
         raise
